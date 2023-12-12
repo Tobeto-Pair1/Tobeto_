2fbd8d99f8d1bc34802b1b764e98afdde1fc5e4b
fatihdemirag <f2000fatih@hotmail.com>
2023-12-12 16:33:55 +0300

    Entity classlari eklendi

---
+++ Entities/Concrete/EducationDetail.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Entities
+{
+	public class EducationDetail:Entity<int>
+	{
+		public DateTime StartedTime { get; set; }
+		public DateTime EstimatedTime { get; set; }
+		public int ContentId { get; set; }
+		public int ManufacturerId { get; set; }
+
+    }
+}
+
---
+++ Entities/Concrete/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+namespace Entities
+{
+	public class Manufacturer :Entity<int>
+	{
+		public string ManufacturerName { get; set; }
+	}
+}
+
