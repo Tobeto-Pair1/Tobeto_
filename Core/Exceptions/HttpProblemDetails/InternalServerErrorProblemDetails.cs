7096ca7e04c4b97fbd71e2fc35b202106eea4a19
fatihdemirag <f2000fatih@hotmail.com>
2023-12-13 18:03:40 +0300

    Exception classes created

---
+++ Core/Exceptions/HttpProblemDetails/InternalServerErrorProblemDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.NetworkInformation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
+
+public class InternalServerErrorProblemDetails : ProblemDetails
+{
+    public InternalServerErrorProblemDetails(string detail)
+    {
+        Title = "Internal Server Error";
+        Detail = "Internal Server Error";
+        Status = StatusCodes.Status500InternalServerError;
+        Type = "https://example.com/probs/internal";
+    }
+}
