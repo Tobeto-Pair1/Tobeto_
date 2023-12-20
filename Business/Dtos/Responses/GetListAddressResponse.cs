97ae90870ec01e0c47ad7aeb205906f22bf99c31
Ayşenur Elmas <ayse66025@gmail.com>
2023-12-20 18:27:47 +0300

    ......

--- Business/Dtos/Responses/GetListAddressResponse.cs
+++ Business/Dtos/Responses/GetListAddressResponse.cs
@@ -8,12 +8,12 @@ namespace Business.Dtos.Responses;
 
 public class GetListAddressResponse
 {
-    public int Id { get; set; }
-    public int CountryId { get; set; }
+    public Guid Id { get; set; }
+    public Guid CountryId { get; set; }
     public string CountryName { get; set; }
-    public int CityId { get; set; }
+    public Guid CityId { get; set; }
     public string CityName { get; set; }
-    public int TownId { get; set; }
+    public Guid TownId { get; set; }
     public string TownName { get; set; }
     public string Description { get; set; }
 }
