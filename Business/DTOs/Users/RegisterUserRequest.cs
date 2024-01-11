47fcb1a0e193ba911bfb821e745464f10e95dc99
Ayşenur Elmas <ayse66025@gmail.com>
2024-01-11 15:47:42 +0300

    security

---
+++ Business/DTOs/Users/RegisterUserRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DTOs.Users;
+
+public class RegisterUserRequest
+{
+    public string FirstName { get; set; }
+    public string Lastname { get; set; }
+    public string  Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+
+}
