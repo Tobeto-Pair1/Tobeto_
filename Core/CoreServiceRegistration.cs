81d3562504b005495ca8c43ab27d4f31fc0fcb2e
fdemirag <f2000fatih@hotmail.com>
2024-02-26 16:53:11 +0300

    Loglama eklendi

--- Core/CoreServiceRegistration.cs
+++ Core/CoreServiceRegistration.cs
@@ -5,6 +5,9 @@ using Core.Utilities.IoC;
 using Core.Utilities.Security.JWT;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Core.CrossCuttingConcerns.Logger.Serilog.Loggers;
+using Core.CrossCuttingConcerns.Logger.Serilog;
+using Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
 
 namespace Core;
 
@@ -17,6 +20,12 @@ public static class CoreServiceRegistration
 
         services.AddScoped<ITokenHelper, JwtHelper>();
         services.AddScoped<IFileUploadAdapter, CloudinaryAdapter>();
+
+        //services.AddScoped<LoggerServiceBase, FileLogger>();
+        //services.AddScoped<LoggerServiceBase, MsSqlLogger>();
+
+        services.AddTransient<FileLogger>();
+        services.AddTransient<MsSqlLogger>();
 
         services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
         ServiceTool.Create(services);
---
+++ Core/CrossCuttingConcerns/Logger/Serilog/ConfigurationModels/MsSqlConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.CrossCuttingConcerns.Logger.Serilog.ConfigurationModels
+{
+    public class MsSqlConfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
+}
